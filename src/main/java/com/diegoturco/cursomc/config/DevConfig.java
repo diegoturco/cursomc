4c72a8c61c48b9b659db9480b4145a086b796342
Diego Turco <dhturco@gmail.com>
2018-06-07 10:43:25 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/diegoturco/cursomc/config/DevConfig.java
+++ src/main/java/com/diegoturco/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.diegoturco.cursomc.services.DBService;
+import com.diegoturco.cursomc.services.EmailService;
+import com.diegoturco.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -31,4 +33,8 @@ public class DevConfig {
 		return true;
 	}
 	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
+	}	
 }
